db9031fc4dbae40d14b4cc01472804017fb3511c
Pablo Muñoz <pablerass@gmail.com>
2016-01-25 21:41:12 +0100

    Add travis configuration and setup options

--- redirector/__init__.py
+++ redirector/__init__.py
@@ -7,16 +7,22 @@ import functools
 import sys
 from io import StringIO
 
-class StandardOutputRedirector(object):
-    """Redirects standard output to a *stream* object.
+__version__ = '0.1.0'
 
-    :param input_stream:
-    """
+class StandardOutputRedirector(object):
+    """Redirects standard output to a *stream* object."""
+
     def __init__(self, input_stream):
+        """Create a redirector which send the standard output to an *input stream*.
+
+        :param input_stream:
+        :type input_steam:
+        """
         self.__input_stream = input_stream
         self.__old_stdout = None
 
     def __enter__(self):
+        """ """
         # Stores current stdout stream
         self.__old_stdout = sys.stdout
         # Redirect output to input_stream
@@ -24,6 +30,7 @@ class StandardOutputRedirector(object):
         return self.__input_stream
 
     def __exit__(self, type_, value, traceback):
+        """ """
         # Revert stdout modification
         self.__input_stream.close()
         sys.stdout = self.__old_stdout
@@ -33,7 +40,7 @@ class StandardOutputRedirector(object):
 
 
 class NullOutputRedirector(StandardOutputRedirector):
-    """Redirects stdout to a null stream.
+    """Redirect stdout to a null stream.
 
     Usage:
 
@@ -42,7 +49,9 @@ class NullOutputRedirector(StandardOutputRedirector):
         with NullOutputRedirector():
             print "Non printed text"
     """
+
     def __init__(self):
+        """ """
         StandardOutputRedirector.__init__(self, self.__DevNull())
 
     class __DevNull(object):
@@ -54,7 +63,7 @@ class NullOutputRedirector(StandardOutputRedirector):
 
 
 class FileOutputRedirector(StandardOutputRedirector):
-    """Redirects stdout to a file.
+    """Redirect stdout to a file.
 
     Usage:
 
@@ -66,28 +75,33 @@ class FileOutputRedirector(StandardOutputRedirector):
     :param str file_name: File to redirect de output.
     :param str mode: Access mode to file.
     """
-    def __init__(self, file_name, mode = "a"):
+
+    def __init__(self, file_name, mode="a"):
         StandardOutputRedirector.__init__(self, open(file_name, mode))
 
 
 class VariableOutputRedirector(StandardOutputRedirector):
-    """Redirect stdout to an ``StringIO`` object that can be used to retrieve
-    the content as an string using ``getvalue()`` method.
+    """Redirect stdout to an ``StringIO`` object.
+
+    The ``StringIO`` object can be used to retrieve the content as an string
+    using ``getvalue()`` method.
 
     Usage:
 
     .. code-block:: python
 
         >>> with VariableOutputRedirector() as out:
-        >>>     print("texto de prueba")
-        >>>     print("otro texto de prueba")
+        >>>     print("test text")
+        >>>     print("another test text")
         >>>     value = out.getvalue()
 
         >>> print value
-        texto de prueba
-        otro texto de prueba
+        test text
+        another test text
     """
+
     def __init__(self):
+        """ """
         StandardOutputRedirector.__init__(self, StringIO())
 
 
@@ -106,11 +120,13 @@ class FunctionOutputRedirector(StandardOutputRedirector):
 
     :param func function: Functino to execute the input.
     """
+
     def __init__(self, function):
-        StandardOutputRedirector.__init__(self, self.__FunctionStream(function))
+        StandardOutputRedirector.__init__(self,
+                                          self.__FunctionStream(function))
 
     class __FunctionStream(object):
-        def __init__(self, function)
+        def __init__(self, function):
             self.__function = function
 
         def write(self, lines):
@@ -122,7 +138,7 @@ class FunctionOutputRedirector(StandardOutputRedirector):
 
 
 class LoggerOutputRedirector(FunctionOutputRedirector):
-    """Redirects stdout to a ``logger`` using the specific log level.
+    """Redirect stdout to a ``logger`` using the specific log level.
 
     Usage:
 
@@ -137,13 +153,16 @@ class LoggerOutputRedirector(FunctionOutputRedirector):
     :param logger logger: Logger to register the output.
     :param int level: Logger register level.
     """
+
     def __init__(self, logger, level):
-        FunctionOutputRedirector.__init__(self, lambda line: logger.log(level, line))
-        FunctionOutputRedirector.__init__(self, functools.partial(logger.log, lvl = level))
-        #StandardOutputRedirector.__init__(self, self.__LoggerStream(logger, level))
+        FunctionOutputRedirector.__init__(self, lambda line: logger.log(level,
+                                                                        line))
+        FunctionOutputRedirector.__init__(self, functools.partial(logger.log,
+                                                                  lvl = level))
 
 
 class GeneratorOutputRedirector(StandardOutputRedirector):
+    """ """
     def __init__(self, logger, level):
         StandardOutputRedirector.__init__(self, self.__GeneratorStream())
 
@@ -154,4 +173,3 @@ class GeneratorOutputRedirector(StandardOutputRedirector):
 
         def close(self):
             pass
-
--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
 
-import datetime
-import os
+from redirector import __version__
+from setuptools import setup, find_packages
 
-import redirector
-
-package_list = []
-for root, dirs, files in os.walk('src'):
-    for dir_ in dirs:
-        if dir_ != '__pycache__':
-            package_list.append(os.path.relpath(root + os.path.sep + dir_, 'src').replace(os.path.sep,'.'))
-
-setup(
-    name = 'Redirector',
-    version = tm.__version__ + 'dev-' + datetime.datetime.now().strftime("%Y%m%d"),
-    #version = tm.__version__ + 'b1',
-    #version = tm.__version__,
-    packages = package_list,
-    package_dir = {'redirector': os.path.join('src','tm')},
+setup(name='redirector',
+      version=__version__,
+      description="Manage and redirect standard output.",
+      long_description=open('README.md').read(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Operating System :: OS Independent',
+      ],
+      keywords='',
+      author='Pablo Muñoz',
+      author_email='pablerass@gmail.com',
+      url='https://github.com/pablerass/redirector',
+      license='LGPLv3',
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[line for line in open('requirements.txt')],
 )
